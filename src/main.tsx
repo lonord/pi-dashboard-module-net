f0b0437575cb804407eb032c95eb12c866c6f2c9
lonord <lonord@qq.com>
2018-03-04 21:02:51 +0800

    加入数据获取模块

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,34 @@
+import createRPCClient, { RPCClient, SSEClient } from '@lonord/pi-status-rpc-client'
 import { Button } from '@lonord/react-electron-components'
 import * as React from 'react'
 import styled from 'styled-components'
 import DetailDialog from './dialog-detail'
+import formatSpeedUnit from './util/speed-formatter'
 
 interface MainPropsMap {
 	selectedIfName: string
+	rpcBaseUrl: string
 }
 
 interface MainProps extends MainPropsMap {
-	updateProps: (props: Pick<MainPropsMap, keyof MainPropsMap>) => void
+	updateProps: <K extends keyof MainPropsMap>(props: Pick<MainPropsMap, K>) => void
 }
 
 interface MainState {
 	isDetailOpen: boolean
+	sendSpeed: number
+	receiveSpeed: number
 }
 
 export default class Main extends React.Component<MainProps, MainState> {
 
+	rpcService: RPCClient = null
+	sseClient: SSEClient = null
+
 	state: MainState = {
-		isDetailOpen: false
+		isDetailOpen: false,
+		sendSpeed: 0,
+		receiveSpeed: 0
 	}
 
 	openDetail = () => {
@@ -34,15 +44,70 @@ export default class Main extends React.Component<MainProps, MainState> {
 	}
 
 	updateIfName = (ifName: string) => {
-		//
+		if (ifName !== this.props.selectedIfName) {
+			this.setState({
+				sendSpeed: 0,
+				receiveSpeed: 0
+			})
+		}
+		this.props.updateProps({
+			selectedIfName: ifName
+		})
+	}
+
+	onSSEData = (data) => {
+		this.setState({
+			sendSpeed: data.send,
+			receiveSpeed: data.receive
+		})
+	}
+
+	initSSE = () => {
+		const { selectedIfName } = this.props
+		if (selectedIfName) {
+			this.sseClient = this.rpcService.openSSE('net-iostat', this.onSSEData, {
+				ifName: selectedIfName
+			})
+		}
+	}
+
+	stopSSE = () => {
+		if (this.sseClient && !this.sseClient.isClosed()) {
+			this.sseClient.close()
+			this.sseClient = null
+		}
+	}
+
+	componentDidUpdate(prevProps: MainProps) {
+		if (prevProps.rpcBaseUrl !== this.props.rpcBaseUrl) {
+			this.stopSSE()
+			this.rpcService = createRPCClient(this.props.rpcBaseUrl)
+			this.initSSE()
+		} else if (prevProps.selectedIfName !== this.props.selectedIfName) {
+			this.stopSSE()
+			this.initSSE()
+		}
+	}
+
+	componentDidMount() {
+		const { rpcBaseUrl, selectedIfName } = this.props
+		this.rpcService = createRPCClient(rpcBaseUrl)
+		this.initSSE()
+	}
+
+	componentWillUnmount() {
+		this.stopSSE()
+		this.rpcService = null
 	}
 
 	render() {
 		const { selectedIfName } = this.props
-		const { isDetailOpen } = this.state
+		const { isDetailOpen, sendSpeed, receiveSpeed } = this.state
 		return (
 			<FullSizeWrap onClick={this.openDetail}>
-				<span>222</span>
+				<div>{selectedIfName}</div>
+				<div>{formatSpeedUnit(sendSpeed)}</div>
+				<div>{formatSpeedUnit(receiveSpeed)}</div>
 				<DetailDialog
 					isOpen={isDetailOpen}
 					onClose={this.closeDetail}
---
+++ src/util/speed-formatter.ts
@@ -0,0 +1,19 @@
+export default function formatSpeedUnit(n: number) {
+	let s = null
+	let unit = null
+	if (n > 1024 * 1024) {
+		s = n / 1024 / 1024 + ''
+		unit = ' MB/s'
+	} else {
+		s = n / 1024 + ''
+		unit = ' KB/s'
+	}
+	const idx = s.indexOf('.')
+	if (idx !== -1) {
+		s += '00'
+		s = s.substr(0, idx + 3)
+	} else {
+		s += '.00'
+	}
+	return s + unit
+}
